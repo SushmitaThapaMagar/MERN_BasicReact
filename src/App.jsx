a6234658de6fa035e246d359563fdfc8a936d395
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-03-10 20:01:40 +0545

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
+import { useEffect, useState } from "react"
 
-import Home from './Home'
-import Blog from './Blog'
-import Contact from './Contact'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes'
-import { use, useState } from 'react'
 
-function App() { //Functional base
-const [count, setCount] = useState(0)
-const [name, setName] =useState("Thapa Magar")
+function App() { 
+  const[count, setCount] = useState(0)
+//first type
+useEffect(() =>{  //useEffect is a hook
+console.log("First UseEffect was called") 
+},[])  //[] : Dependency Array
 
+
+
+//second type
+useEffect (() =>{
+  console.log("Count UseEffect was called")
+}, [count])
 
   return (
 <>
 <h1>{count}</h1>
-<button onClick={()=> setCount(count +1)}>+</button>
-<button onClick={()=> setCount(count -1)}>-</button>
-<h1>{name}</h1>
-<button onClick={()=> setName("Sushmita Thapa Magar")}>Change Name</button>
+<button onClick={()=>setCount(count+1)}>+</button>
 </>
     
   )
